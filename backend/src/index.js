39414143e9fb3be26e683b16b5c94db4411d47f0
jefersonrosa <jsrgodoy@gmail.com>
2024-04-30 00:23:59 -0300

    criando rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,9 +12,35 @@ const Music = mongoose.model('Music', {
     artist: String
 })
 
+//Crianto rotas
+
 app.get("/", async (req, res) => {
     const music = await Music.find()
     return res.send(music)
+})
+
+app.get("/search", async (req, res) => {
+    // Extrai os parâmetros de consulta da requisição
+    const { name, artist } = req.query
+
+    // Constrói o objeto de filtro com base nos parâmetros fornecidos
+    const filter = {};
+    if (name) {
+        filter.name = { $regex: name, $options: "i" } // Realiza uma pesquisa por nome, ignorando maiúsculas e minúsculas
+    }
+    if (artist) {
+        filter.artist = { $regex: artist, $options: "i" } // Realiza uma pesquisa por artista, ignorando maiúsculas e minúsculas
+    }
+
+    try {
+        // Executa a consulta usando o filtro construído
+        const music = await Music.find(filter)
+        return res.send(music);
+    } catch (error) {
+        // Trata erros de consulta
+        console.error("Erro ao buscar registros:", error)
+        return res.status(500).send("Erro ao buscar registros.");
+    }
 })
 
 app.delete("/:id", async(req, res) =>{
